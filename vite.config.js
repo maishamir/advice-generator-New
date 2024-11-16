3f5f2cea90b9aa52ce58fd5d555c53604c500bae
maishamir <45680002+maishamir@users.noreply.github.com>
2024-11-15 18:07:43 -0800

    Fix base path with trailing slash

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/advice-generator-New/",
+  base: "/advice-generator-New/", // Ensure the trailing slash is present
   plugins: [react()],
-})
+});
